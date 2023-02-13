50aa780972acd0ca147056a79e404a1142107bce
evertonpsantos <evertonpsantos8@gmail.com>
2023-02-13 17:47:13 -0300

    feat: adds CarController

---
+++ src/Controllers/CarController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import ICar from '../Interfaces/ICar';
+import CarService from '../Services/CarService';
+
+export default class CarController {
+  private _service: CarService;
+
+  constructor(
+    private req: Request, 
+    private res: Response,
+  ) {
+    this._service = new CarService();
+  }
+
+  public registerNewCar = async () => {
+    const newCarRequest: Omit<ICar, 'id'> = this.req.body;
+
+    const result = await this._service.registerNewCar(newCarRequest);
+    return this.res.status(201).json(result);
+  };
+}
