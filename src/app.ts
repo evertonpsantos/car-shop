4617960e965cb8a4cb3d77ab20d6ebfc74975bc5
evertonpsantos <evertonpsantos8@gmail.com>
2023-02-13 17:59:55 -0300

    feat: add /cars route to app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
+import carsRouter from './Routes/CarRouter';
 
 const app = express();
 app.use(express.json());
+app.use('/cars', carsRouter);
 
 export default app;
