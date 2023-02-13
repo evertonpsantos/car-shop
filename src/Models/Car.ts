bebbc0877ef1ff0bc7a73a379665592eb7b0ac93
evertonpsantos <evertonpsantos8@gmail.com>
2023-02-13 17:25:18 -0300

    feat: creates Car ODM

---
+++ src/Models/Car.ts
@@ -0,0 +1,26 @@
+import { model, Model, models, Schema } from 'mongoose';
+import ICar from '../Interfaces/ICar';
+
+export default class Car {
+  private schema: Schema;
+  private model: Model<ICar>;
+
+  constructor() {
+    this.schema = new Schema({
+      id: { type: String },
+      model: { type: String, required: true },
+      year: { type: Number, required: true },
+      color: { type: String, required: true },
+      status: { type: Boolean },
+      buyValue: { type: Number, required: true },
+      doorsQty: { type: Number, required: true },
+      seatsQty: { type: Number, required: true },
+    });
+
+    this.model = models.Car || model('Car', this.schema);
+  }
+
+  public async registerNewCar(newCar: Omit<ICar, 'id'>): Promise<ICar> {
+    return this.model.create({ ...newCar });
+  }
+}
