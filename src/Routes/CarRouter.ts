7e5f11ef0fa5ab52ac622bb5bc2a95bb7aab2c41
evertonpsantos <evertonpsantos8@gmail.com>
2023-02-14 13:22:50 -0300

    feat: add endpoint /cars to get car list

--- src/Routes/CarRouter.ts
+++ src/Routes/CarRouter.ts
@@ -4,5 +4,6 @@ import CarController from '../Controllers/CarController';
 const carsRouter = Router();
 
 carsRouter.post('/', (req, res) => new CarController(req, res).registerNewCar());
+carsRouter.get('/', (req, res) => new CarController(req, res).getAllCars());
 
 export default carsRouter;
