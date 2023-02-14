dced930c086fde764e9956d1242d06be471be767
evertonpsantos <evertonpsantos8@gmail.com>
2023-02-14 16:20:19 -0300

    chore: changing Car domain class

--- src/Domains/Car.ts
+++ src/Domains/Car.ts
@@ -20,54 +20,6 @@ class Car {
     this.doorsQty = newCar.doorsQty;
     this.seatsQty = newCar.seatsQty;
   }
-
-  // set id(id: string) {
-  //   this._id = id;
-  // }
-
-  // get id() {
-  //   return this._id;
-  // }
-
-  // set model(model: string) {
-  //   this._model = model;
-  // }
-
-  // get model() {
-  //   return this._model;
-  // }
-
-  // set year(year: number) {
-  //   this._year = year;
-  // }
-
-  // get year() {
-  //   return this._year;
-  // }
-
-  // set color(color: string) {
-  //   this._color = color;
-  // }
-
-  // get color() {
-  //   return this._color;
-  // }
-
-  // set status(status: boolean) {
-  //   this._status = status;
-  // }
-
-  // get status() {
-  //   return this._status;
-  // }
-
-  // set buyValue(buyValue: number) {
-  //   this._buyValue = buyValue;
-  // }
-
-  // get buyValue() {
-  //   return this._buyValue;
-  // }
 }
 
 export default Car;
